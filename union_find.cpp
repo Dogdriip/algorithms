579de13094eac50a852ec17c8f0e090175b9d7e9
Hyunseung Jeon <dogdriip@gmail.com>
2020-03-23 00:51:59 +0900

    윾과

---
+++ union_find.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int par[200001];  // uf
+
+int Find(int x) {
+    if (parent[x] < 0) return x;
+    return parent[x] = Find(parent[x]);
+}
+void Union(int x, int y) {
+    x = Find(x); y = Find(y);
+    if (x == y) return;
+    parent[x] += parent[y];
+    parent[y] = x;
+}
+
+// 루트 정점은 par[]가 음수이면서 그 값은 -(원소 개수)가 됨
+// a번 원소가 들어있는 집합의 크기 == -par[Find(a)]
