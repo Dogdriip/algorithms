779e4ae758a39650d33e2fd8ed1759bae2f99c38
Hyunseung Jeon <dogdriip@gmail.com>
2020-12-10 13:35:45 +0900

    Update description of Edmonds-Karp

--- EdmondsKarp.cpp
+++ EdmondsKarp.cpp
@@ -23,6 +23,8 @@ int main() {
         int v = (_v <= 'Z' ? _v - 'A' : _v - 'a' + 26);
 
         // 같은 간선이 여러 번 들어올 수 있으므로 누적
+        // 단, capacity는 이 문제에서만 양방향.
+        // 단방향에서는 capacity만 단방향이고, 간선은 양방향으로 추가해주면 된다.
         c[u][v] += w;
         c[v][u] += w;
         adj[u].push_back(v);
@@ -36,36 +38,35 @@ int main() {
     // 증가 경로(Augmenting path)를 못 찾을 때까지 루프
     while (true) {
         // Augmenting path를 BFS로 찾는다
-        int prev[MAX_V+1];
-        memset(prev, -1, sizeof(prev));
+        vector<int> par(MAX_V+1, -1);
         queue<int> q;
         q.push(S);
-        while (!q.empty() && prev[E] == -1) {
+        while (!q.empty() && par[E] == -1) {
             int x = q.front(); q.pop();
             for (int nx : adj[x]) {
                 // c[i][j] - f[i][j] > 0 : i에서 j로 유량을 흘릴 여유가 남았는가?
-                // prev[nx] == -1 : nx를 아직 방문하지 않았는가?
-                if (c[x][nx] - f[x][nx] > 0 && prev[nx] == -1) {
+                // par[nx] == -1 : nx를 아직 방문하지 않았는가?
+                if (c[x][nx] - f[x][nx] > 0 && par[nx] == -1) {
                     q.push(nx);
-                    prev[nx] = x;  // 경로를 기억하기 위해 prev 배열 사용
+                    par[nx] = x;  // 경로를 기억하기 위해 par (parent) 배열 사용
                     if (nx == E) break;  // Sink에 도달하면 break
                 }
             }
         }
 
         // Sink로 가는 경로가 더 없으면 루프 탈출
-        if (prev[E] == -1) break;
+        if (par[E] == -1) break;
 
         // 경로를 따라가면서 가장 허용치가 낮은 곳을 찾음
         int flow = INF;
-        for (int i = E; i != S; i = prev[i]) {
-            flow = min(flow, c[prev[i]][i] - f[prev[i]][i]);
+        for (int i = E; i != S; i = par[i]) {
+            flow = min(flow, c[par[i]][i] - f[par[i]][i]);
         }
 
         // 찾은 flow만큼 해당 경로에 유량을 흘려준다
-        for (int i = E; i != S; i = prev[i]) {
-            f[prev[i]][i] += flow;
-            f[i][prev[i]] -= flow;
+        for (int i = E; i != S; i = par[i]) {
+            f[par[i]][i] += flow;
+            f[i][par[i]] -= flow;
         }
 
         // 총 유량 합 갱신
